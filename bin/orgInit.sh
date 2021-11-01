2b8cf6d43f86046888c879e0b33575d8df7c6e0b
Kevin Poorman <kpoorman@salesforce.com>
2021-11-01 10:40:59 -0400

    updates to OrgShape pull in chatter features

---
+++ bin/orgInit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DURATION=7
+
+if [ "$#" -eq 1 ]; then
+  DURATION=$1
+fi
+
+sfdx force:org:create -f config/project-scratch-def.json --setalias $(git symbolic-ref --short -q HEAD) --durationdays 7 --setdefaultusername --json --loglevel fatal
+sfdx force:source:push
+echo "Org is set up"
